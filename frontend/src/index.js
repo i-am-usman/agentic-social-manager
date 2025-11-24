29e5ecda33fa61b97d4b61276afb937dff1268a3
i-am-usman <siyalmusman@gmail.com>
2025-11-24 23:00:06 +0500

    Added reportWebVitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,8 @@ root.render(
   <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Log metrics in development only (avoid noise in production)
+// In production you can replace console.log with a function that POSTs metrics to your analytics endpoint.
+if (process.env.NODE_ENV === 'development') {
+  reportWebVitals(console.log);
+}
